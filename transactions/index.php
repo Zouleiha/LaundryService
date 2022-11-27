8381555e9346013b76b5a5db4bbf0e2aa6d65bc5
Abubakar-245 <gandega@iut-dhaka.edu>
2022-11-27 18:27:53 +0600

    with transactions table

--- transactions/index.php
+++ transactions/index.php
@@ -1,8 +1,35 @@
 <?php include('./../inc/customerHeader.php'); ?>
 
-<?php 
-    echo "View recent transactions here"; 
-?>
+    <table>
+        <thead>
+            <tr>
+                <td>Date</td>
+                <td>Amount required</td>
+                <td>Amount paid</td>
+            </tr>
+        </thead>
+
+        <tbody>
+        
+        <?php
+            $result = $system->getTableContent('transactions');
+            $id = 0;
+
+            while($row = $result->fetch_assoc()){
+                $id = $id + 1;
+                $date_of_transaction = $row["date_of_transaction"];
+                $amount_required = $row["amount_required"];
+                $amount_paid = $row["amount_paid"];
+
+                echo "<tr>
+                        <td>$date_of_transaction</td>
+                        <td>$amount_required</td>
+                        <td>$amount_paid</td>
+                    </tr>";
+            }
+        ?>
+        </tbody>
+    </table>
 
 
 <?php include('./../inc/customerFooter.php'); ?>
