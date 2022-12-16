2e574cf23e22cf3f94ec05c921795250f31d7709
Abubakar-245 <gandega@iut-dhaka.edu>
2022-12-16 12:19:36 +0600

    commit

--- transactions/index.php
+++ transactions/index.php
@@ -1,35 +1,43 @@
 <?php include('./../inc/customerHeader.php'); ?>
 
-    <table>
-        <thead>
-            <tr>
-                <td>Date</td>
-                <td>Amount required</td>
-                <td>Amount paid</td>
-            </tr>
-        </thead>
+<div class="details">
+                <div class="recentOrders">
+                    <div class="cardHeader">
+                        <h2>Transactions</h2>
+                    </div>
 
-        <tbody>
-        
-        <?php
-            $result = $system->getTableContent('transactions');
-            $id = 0;
+                    <table>
+                        <thead>
+                            <tr>
+                                <td>Date</td>
+                                <td>Amount required</td>
+                                <td>Amount paid</td>
+                            </tr>
+                        </thead>
 
-            while($row = $result->fetch_assoc()){
-                $id = $id + 1;
-                $date_of_transaction = $row["date_of_transaction"];
-                $amount_required = $row["amount_required"];
-                $amount_paid = $row["amount_paid"];
+                        <tbody>
+                        
+                        <?php
+                            $result = $system->getTableContent('transactions');
+                            $id = 0;
 
-                echo "<tr>
-                        <td>$date_of_transaction</td>
-                        <td>$amount_required</td>
-                        <td>$amount_paid</td>
-                    </tr>";
-            }
-        ?>
-        </tbody>
-    </table>
+                            while($row = $result->fetch_assoc()){
+                                $id = $id + 1;
+                                $date_of_transaction = $row["date_of_transaction"];
+                                $amount_required = $row["amount_required"];
+                                $amount_paid = $row["amount_paid"];
+
+                                echo "<tr>
+                                        <td>$date_of_transaction</td>
+                                        <td>$amount_required</td>
+                                        <td>$amount_paid</td>
+                                    </tr>";
+                            }
+                        ?>
+                        </tbody>
+                    </table>
+                   
+                </div>
 
 
 <?php include('./../inc/customerFooter.php'); ?>
