3943b3667576431f619480f63dde83621bee52fe
Abubakar-245 <gandega@iut-dhaka.edu>
2022-12-20 17:48:03 +0600

    ocm

---
+++ admin/customers.php
@@ -0,0 +1,41 @@
+<div class="details">
+                <div class="recentOrders">
+                    <div class="cardHeader">
+                        <h2>Recent Orders</h2>
+                        <a href="#" class="btn">View All</a>
+                    </div>
+
+                    <table>
+                        <thead>
+                            <tr>
+                                <td>Date</td>
+                                <td>Amount required</td>
+                                <td>Amount paid</td>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                        
+                        <?php
+                            $result = $system->getTableContent('transactions');
+                            $id = 0;
+
+                            while($row = $result->fetch_assoc()){
+                                $id = $id + 1;
+                                $date_of_transaction = $row["date_of_transaction"];
+                                $amount_required = $row["amount_required"];
+                                $amount_paid = $row["amount_paid"];
+
+                                echo "<tr>
+                                        <td>$date_of_transaction</td>
+                                        <td>$amount_required</td>
+                                        <td>$amount_paid</td>
+                                    </tr>";
+                            }
+                        ?>
+                        </tbody>
+                    </table>
+                   
+                </div>
+                
+            </div>
